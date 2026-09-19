f670b23e5d96c3114b2329bb285ed00588e9beea
Himanshu Sharma <152473030+CodeXGautam@users.noreply.github.com>
2026-09-19 17:18:33 +0530

    Export config for middleware matcher
    Added export config for middleware matching regex.

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,6 +60,7 @@ export default auth((req) => {
   return NextResponse.next();
 });
 
+// export config matching regrex
 export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon\\.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
